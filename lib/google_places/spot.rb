4a33bc0dfbfca9c3c404daf64446b5ddedbc5339
Anirudh Kalkotwar <anirudh.kkw@gmail.com>
2015-09-27 04:30:23 +0530

    added some params

--- lib/google_places/spot.rb
+++ lib/google_places/spot.rb
@@ -1,7 +1,7 @@
 require 'google_places/review'
 module GooglePlaces
   class Spot
-    attr_accessor :lat, :lng, :viewport, :name, :icon, :reference, :vicinity, :types, :id, :formatted_phone_number, :international_phone_number, :formatted_address, :address_components, :street_number, :street, :city, :region, :postal_code, :country, :rating, :url, :cid, :website, :reviews, :aspects, :zagat_selected, :zagat_reviewed, :photos, :review_summary, :nextpagetoken, :price_level, :opening_hours, :events, :utc_offset, :place_id, :photo_url
+    attr_accessor :lat, :lng, :viewport, :name, :icon, :reference, :vicinity, :types, :id, :formatted_phone_number, :international_phone_number, :formatted_address, :address_components, :street_number, :street, :city, :region, :postal_code, :country, :rating, :url, :cid, :website, :reviews, :aspects, :zagat_selected, :zagat_reviewed, :photos, :review_summary, :nextpagetoken, :price_level, :opening_hours, :events, :utc_offset, :place_id, :photo_url, :distance, :travel_time_in_minutes, :amount_min, :amount_max
 
     # Search for Spots at the provided location
     #
